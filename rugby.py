9cbdbea3b9c3f9483ba68938831cc2662338601a
e-coucou <eric@ecoucou.com>
2015-10-13 19:34:50 +0200

    Maj Event

--- rugby.py
+++ rugby.py
@@ -15,8 +15,15 @@ def get_rank(json,date):
         if CSV:
             print "{3:};{0:d};{1:};{7:};{2:.1f};{4:d};{5:.1f};{6:d}".format(e['pos'],e['team']['name'], e['pts'] , date, e['matches'], e['previousPts'], e['previousPos'], e['team']['abbreviation'])
         else:
-            print "{3:11}{0:3d} {1:37}{7:5}{2:5.1f}{4:6d}{5:7.1f}{6:6d}".format(e['pos'],e['team']['name'], e['pts'] , date, e['matches'], e['previousPts'], e['previousPos'], e['team']['abbreviation'])
+            print "{3:11}{0:3d} {1:37}{7:5}{2:5.2f}{4:6d}{5:7.2f}{6:6d}".format(e['pos'],e['team']['name'], e['pts'] , date, e['matches'], e['previousPts'], e['previousPos'], e['team']['abbreviation'])
 
+def get_rk(json,ABB):
+	for f in json:
+		if f['team']['abbreviation'] == ABB : 
+			return f
+			break
+	return 0
+			
 def get_all_csv(base):
     entete()
     oMaj, maj = '', ''
@@ -59,10 +66,7 @@ def get_info(crit,dict):
             break
 
 def get_result(l_pts,v_pts,l_score,v_score,wc):
-    if wc :
-    	delta = -min(10,l_pts-v_pts)*0.1
-    else:
-    	delta = -min(10,l_pts+3-v_pts)*0.1
+    delta = -min(10,l_pts+3*(2-wc)-v_pts)*0.1
     l,v = delta, -delta
     if (l_score > v_score):
         l = l + 1
@@ -70,14 +74,47 @@ def get_result(l_pts,v_pts,l_score,v_score,wc):
     elif (l_score<v_score):
         l = l - 1
         v = v + 1
-    if wc :
-        l = 2 * l
-        v = 2 * v
     if abs(l_score-v_score)>15 :
         l = 1.5 * l
         v = 1.5 * v
-    return l_pts+l, v_pts+v
+    return l_pts+l*wc, v_pts+v*wc
 
+def get_event(url):
+	startDate = str(raw_input('Date de debut [2015-01-01] :')) or '2015-01-01'
+	endDate=str(raw_input('Date de fin [2015-12-31] :')) or '2015-12-31'
+	pageSize='100'
+	page=0
+	u = url + '?startDate='+startDate+'&endDate='+endDate+'&pageSize='+pageSize+'&page='+str(page)
+	print u
+	j = get_data(u)
+	for e in j['content']:
+		if e['sport'] == args.source : print "{0:5d} - {1:60}  ({2:} / {3:})".format(int(e['id']),e['label'].encode('windows-1252'),e['start']['label'],e['end']['label'])
+	print
+	event = str(raw_input("Choisir le numero de l'evenement [1238]: ")) or '1238'
+	u = url+'/'+event+'/schedule?language=en&client=pulse'+'&status=C'
+	j = get_data(u)
+	print '-------------------------------------------'
+	print "{0:} du {1:} au {2:}\n-".format(j['event']['label'], j['event']['start']['label'], j['event']['end']['label'])
+	for e in j['matches']:
+		t = time.gmtime(e['time']['millis']/1000+7200)
+		d= time.strftime('%Y-%m-%d', t)
+		u_rk = 'http://cmsapi.pulselive.com/rugby/rankings/'+args.source+'?date='+d
+		j_rk = get_data(u_rk)
+		f0 = get_rk(j_rk['entries'],e['teams'][0]['abbreviation'])
+		f1 = get_rk(j_rk['entries'],e['teams'][1]['abbreviation'])
+		s0,s1=0,0
+		p0,p1 = 0,0
+		if f0 : 
+			p0=f0['pts']
+		if f1 : 
+			p1=f1['pts']
+		if e['status']== 'C' :
+			s0,s1 = get_result(p0,p1,e['scores'][0],e['scores'][1],j['event']['rankingsWeight'])
+		print "{2:2}{0:13} {3:3} -{4:3} {1:13}{7:14} {5:} {8:5} ({9:4.1f}-{10:4.1f})/({11:4.1f}-{12:4.1f}) {6:}".format(e['teams'][0]['name'],e['teams'][1]['name'],e['status'],e['scores'][0],e['scores'][1], time.strftime('%d/%m %H:%M', t),e['venue']['name'],e['eventPhase'],e['attendance'],p0,p1,s0,s1)
+	print '-------------------------------------------'
+	return
+
+	
 def get_args():
     global p, args
     global DEBUG, OUTSIDE, VERBOSE, CSV
@@ -85,7 +122,7 @@ def get_args():
     parser = argparse.ArgumentParser(description='Recupere les historiques de classement de l IRB depuis 2003')
     parser.add_argument("-p","--proxy",default='proxy.my-org.local:8080',help="si vous etes derriere un proxy =1")
     parser.add_argument("-s","--source",default='mru',choices=['mru','wru'],help="mru/wru pour men ou wemen")
-    parser.add_argument("-m","--mode",default='maj',choices=['maj','score','all','date'],help="maj : le classement actuel, score : evaluer un classement, all : tous les classements, date : classement à une date option -D")
+    parser.add_argument("-m","--mode",default='maj',choices=['maj','score','all','date', 'event'],help="maj : le classement actuel, score : evaluer un classement, all : tous les classements, date : classement à une date option -D")
     parser.add_argument("-d","--debug",default=False,action="store_true", help="=1 en mode DEBUG" )
     parser.add_argument("-D","--Date", default='9999-12-31',help="Date de la demande : aaaa-mm-jj" )
     parser.add_argument("-O","--Outside",default= False , action="store_true" ,help="par defaut NO Proxy")
@@ -143,13 +180,18 @@ if __name__ == "__main__":
             print "[{1:3}] - {0:35}[{3:3}] - {2:35}".format(local['team']['name'],local['pos'],visiteur['team']['name'],visiteur['pos'])
             print "       {0:5.1f}                                      {1:5.1f}".format(local['pts'],visiteur['pts'])
             print "    ({0:5d})                                    ({1:5d})".format(int(score[0]),int(score[1]))
-            v,l = get_result(local['pts'],visiteur['pts'],int(score[0]),int(score[1]), True)
+            v,l = get_result(local['pts'],visiteur['pts'],int(score[0]),int(score[1]), 2)
             print "       {0:5.2f}                                      {1:5.2f}".format(v,l)
     elif mode == 'all' :
         get_all_csv(u)
     elif mode == 'input' :
         print args.input
+    elif mode == 'event' :
+		get_event('http://cmsapi.pulselive.com/rugby/event')
     print "\n\n\nby e-Coucou 2015"
 """    for e in j['entries']:
         print e['team']['abbreviation']
+		http://cmsapi.pulselive.com/rugby/match/14232/timeline?language=en&client=pulse  timeline info
+		http://cmsapi.pulselive.com/rugby/match/14232/stats?language=en&client=pulse stat info
+		http://cmsapi.pulselive.com/rugby/match/14232/summary?language=en&client=pulse info
 """
