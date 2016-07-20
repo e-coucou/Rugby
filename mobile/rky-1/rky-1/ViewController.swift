9673f7da301e13f05a52d478e04cd3e213976efe
e-coucou <eric@ecoucou.com>
2016-07-20 23:23:22 +0200

    first

---
+++ mobile/rky-1/rky-1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rky-1
+//
+//  Created by Eric PLAIDY on 10/05/2016.
+//  Copyright © 2016 eCoucou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
